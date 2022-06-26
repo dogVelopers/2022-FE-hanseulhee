a9663d505ef5d2076a2eb7a8bcccae2d33f9471a
hanseulhee <3021062@gmail.com>
2022-06-26 17:21:06 +0900

    feat: apply countUp, styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
 function App() {
+  const [number, setNumber] = useState<number>(1);
   // option shift F
-  const d = 'd';
-  return <h1>아녕</h1>;
+  return (
+    <div>
+      <StyledH1 num={number} backgroundColor="pink">
+        안녕
+      </StyledH1>
+      <p>{number}</p>
+      <button onClick={() => setNumber(prev => prev + 1)}>up</button>
+    </div>
+  );
 }
 
 export default App;
+
+interface StyledH1Props {
+  num: number;
+  backgroundColor: string;
+}
+
+const StyledH1 = styled.h1<StyledH1Props>`
+  /* color: ${props => (props.num % 2 === 0 ? 'red' : 'blue')}; */
+  color: ${({ num }) => (num % 2 === 0 ? 'yellow' : 'blue')};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  font-size: ${props => props.num}px;
+`;
