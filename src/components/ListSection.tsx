da8a89eb4020e39033e91646adcce8e47cc59713
hanseulhee <3021062@gmail.com>
2022-07-07 19:13:40 +0900

    feat: get list in ListSection

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -1,4 +1,24 @@
-export default function ListSection(){
-    // 리스트 렌더링 해오기
-    return <section></section>
+import { useEffect, useState } from 'react';
+import { instance } from '../libs/api';
+
+function ListSection() {
+  const [data, setData] = useState<any>({ records: [] });
+
+  useEffect(() => {
+    async function getList() {
+      const result = await instance.get('/todo');
+      setData(result.data);
+    }
+    getList();
+  }, []);
+
+  return (
+    <section>
+      {data.records.map(record => (
+        <span key={record.id}>{record.fields.name}</span>
+      ))}
+    </section>
+  );
 }
+
+export default ListSection;
