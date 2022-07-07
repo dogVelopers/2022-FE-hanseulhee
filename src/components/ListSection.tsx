54b8288143bf8dffc4c06339071226730ce9e97d
hanseulhee <3021062@gmail.com>
2022-07-07 20:48:03 +0900

    feat: apply interface

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from 'react';
 import { instance } from '../libs/api';
 
+interface FieldsInterface {
+  name: string;
+}
+
+interface IList {
+  id: string;
+  createdTime: string;
+  fields: FieldsInterface;
+}
+
 function ListSection() {
-  const [data, setData] = useState<any>({ records: [] });
+  const [data, setData] = useState<IList[]>([]);
 
   useEffect(() => {
     async function getList() {
       const result = await instance.get('/todo');
-      setData(result.data);
+      setData(result.data.records);
     }
     getList();
   }, []);
 
   return (
     <section>
-      {data.records.map(record => (
+      {data.map(record => (
         <span key={record.id}>{record.fields.name}</span>
       ))}
     </section>
