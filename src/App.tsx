55bd229b6e3b99d977d6302094e807153e3952f7
hanseulhee <3021062@gmail.com>
2022-07-08 20:38:13 +0900

    feat: apply props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import InputSection from './components/InputSection';
 import ListSection from './components/ListSection';
+import useTodos from './hooks/api/useTodos';
 
 function App() {
   // option shift F
+  const { createTodo, todos } = useTodos();
   return (
     <>
-      <InputSection />
-      <ListSection />
+      <InputSection createTodo={createTodo} />
+      <ListSection todos={todos} />
     </>
   );
 }
