390d53d50afd5f7f09b9d01cd12c30cec4f207db
hanseulhee <3021062@gmail.com>
2022-07-24 20:19:34 +0900

    feat: apply Detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { instance } from '../libs/api';
+import styled from 'styled-components';
+import colors from '../styles/Colors';
+import font from '../styles/Font';
+import { Link } from 'react-router-dom';
+import Nav from '../components/Nav';
+import Terminal from '../components/Detail/Terminal';
 
 function Detail() {
   const [todo, setTodo] = useState<ITodo | null>(null);
@@ -13,13 +19,53 @@ function Detail() {
     }
     saveTodo();
   }, []);
+
   return (
-    <div>
-      Detail page
-      <p>{todoId}</p>
-      <p>{todo?.fields.name}</p>
-    </div>
+    <StyledItemWrapper>
+      <Nav title={todo?.fields.name} />
+      <StyledItemInWrapper>
+        <Terminal todos={todo?.fields.name} />
+      </StyledItemInWrapper>
+    </StyledItemWrapper>
   );
 }
 
 export default Detail;
+
+const StyledItemWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10%;
+`;
+
+const StyledItemInWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const StyledContentWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
+`;
+
+const StyledIntroText = styled.h1`
+  font-size: 1.3rem;
+  letter-spacing: 0.02px;
+  font-weight: ${font.bold};
+`;
+
+const StyledSubSummary = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.8rem;
+  font-weight: ${font.light};
+  margin: 1rem 0 1.6rem 0;
+`;
